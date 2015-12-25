44ecb760a1037274e7d6eb683ea138b6f2ec9ff4
Ashansa Perera <ashansa.perera@gmail.com>
2015-12-25 23:33:13 +0100

    Update experiment.rb

--- recipes/experiment.rb
+++ recipes/experiment.rb
@@ -15,31 +15,6 @@ echo "$(date)" > /home/vagrant/time2.txt
 end
 
 
-file "#{node[:hibench][:home]}/conf/99-user_defined_properties.conf" do 
-  owner node[:hibench][:user]
-  action :delete
-end
-
-template "#{node[:hibench][:home]}/conf/99-user_defined_properties.conf" do
-    source "99-user_defined_properties.conf.erb"
-    owner node[:hibench][:user]
-    group node[:hibench][:group]
-    mode 0775
-end
-
-script 'prepare' do
-  cwd "/tmp"
-  user node['hibench']['user']
-  group node['hibench']['group']
-  interpreter "bash"
-  code <<-EOM
-  cd #{node[:hibench][:home]}
-  ./workloads/streamingbench/prepare/initTopic.sh
-  ./workloads/streamingbench/prepare/genSeedDataset.sh
-  ./workloads/streamingbench/prepare/gendata.sh
-  EOM
-end
-
 # echo "checkout HiBench repo"
 # sudo apt-get install maven
 # echo "maven installed"
