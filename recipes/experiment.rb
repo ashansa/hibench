e735962c41f073d995350342d1e4fdfe9e647931
Ashansa Perera <ashansa.perera@gmail.com>
2015-12-25 15:44:30 +0100

    Update experiment.rb

--- recipes/experiment.rb
+++ recipes/experiment.rb
@@ -8,6 +8,7 @@ script 'run_experiment' do
   cwd "/tmp/hibench"
   echo "$(date)" > /home/vagrant/time.txt
 wget https://s3-eu-west-1.amazonaws.com/hibench-compiled-jar/HiBench-spark-1.3.zip
+unzip HiBench-spark-1.3.zip
 echo "downloaded hibench"
 echo "$(date)" > /home/vagrant/time2.txt
   EOM
